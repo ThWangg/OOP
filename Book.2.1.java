0b566cfab795512d4f736f3b554f3eb82b89c8fd
Admin <quangphanthanhnguyen@gmail.com>
2024-10-20 23:31:36 +0700

    2.1

--- Book.2.1.java
+++ Book.2.1.java
@@ -1,14 +1,16 @@
-public class Book{
-    String name;
-    Author authors;
-    double price;
-    int qty = 0;
+public class Book
+{
+    private String name;
+    private Author authors;
+    private double price;
+    private int qty = 0;
     public Book(String name, Author author, double price)
     {
         this.name = name;
         this.author = author;
         this.price = price;
     }
+    
     public Book(String name, Author author, double price, int qty)
     {
         this.name = name;
@@ -16,27 +18,37 @@ public class Book{
         this.price = price;
         this.qty = qty;
     }
-    public String getName(){
+    
+    public String getName()
+    {
         return name;
     }
-    public Author getAuthor(){
+
+    public Author getAuthor()
+    {
         return author;
     }
-    public double getPrice(){
+
+    public double getPrice()
+    {
         return price;
     }
+
     public void setPrice(double price)
     {
         this.price = price;
     }
+
     public int getQty()
     {
         return qty;
     }
+
     public void setQty(int qty)
     {
         this.qty = qty; 
     }
+
     public String toString()
     {
         return "Book[name = " + name + ", " + author.toString() + ", price = " + price + ", qty = " + qty + "]";
